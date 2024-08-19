6bf83eb4e8b898d0f14ab62ac3e9dba3978c7569
Ranganathan Kasiganesan <ranganath73@gmail.com>
2024-08-19 09:27:36 +0530

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	Location.java

---
+++ my_island/Island.java
@@ -0,0 +1,10 @@
+package my_island.my_island;
+
+import my_island.my_island.Location;
+import my_island.my_island.animal.carnivores.Hunter;
+import my_island.my_island.animal.herbivores.PlantEater;
+import my_island.my_island.plants.Plant;
+
+public class Island <L extends Location, C extends  Hunter, H extends PlantEater, P extends Plant>{
+
+}
--- my_island/Location.java
+++ my_island/Location.java
@@ -1,7 +1,15 @@
 package my_island.my_island;
 
 public class Location {
-    public boolean hasSuitableFood() {
+    private int latitude;
+    private int longitude;
+
+    public Location(int latitude, int longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public static boolean hasSuitableFood() {
             return ((int)(Math.random() * 10)) == 0 ? false : true;
         }
 }
